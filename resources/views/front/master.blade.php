56c6715441401a54bcfcb4bdcce232f2e508b65a
ahmadothman1981 <ahmadothman@outlook.com>
2024-08-23 21:19:29 +0300

    divide front templete to sections and includes in master blade

---
+++ resources/views/front/master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('front.partials.head')
+
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        <!-- Spinner Start -->
+         @include('front.partials.spinner')
+       
+
+        <!-- Navbar & Hero Start -->
+        <div class="container-xxl position-relative p-0">
+            @include('front.partials.navbar')
+           
+            @yield('hero')
+            
+            </div>
+        </div>
+        <!-- Navbar & Hero End -->
+
+                @yield('content')
+              
+        
+        @include('front.partials.footer')
+        <!-- Footer Start -->
+       
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+    @include('front.partials.scripts')
+    <!-- JavaScript Libraries -->
+   
+</body>
+
+</html>
